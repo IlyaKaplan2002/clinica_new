b274ce18714df329b1663185bdceac0b949ff4cb
Misha2048 <kaplan11092002@gmail.com>
2021-07-30 14:58:26 +0300

    ...

--- js/index.js
+++ js/index.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
   let items = $(".info-item");
-  console.log(items);
   for(let i = 1; i <= 5; i++) {
     $(`.open[name=b${i}]`).bind("click", function() {
       for(let j = 0; j < items.length; j++) {
@@ -13,5 +12,4 @@ $(document).ready(function() {
       $(`.info-item[name=li${i}]`).attr("class", "info-item closed");
     });
   }
-  $(".info-item");
 });
