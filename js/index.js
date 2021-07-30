d2bbebdb498a64f4315d4cea0b8b1d4992daa6c9
Misha2048 <kaplan11092002@gmail.com>
2021-07-30 14:09:18 +0300

    js added

--- js/index.js
+++ js/index.js
@@ -1,2 +1,10 @@
-
-
+$(document).ready(function() {
+  for(let i = 1; i <= 5; i++) {
+    $(`.open[name=b${i}]`).bind("click", function() {
+      $(`.info-item[name=li${i}]`).attr("class", "info-item opened");
+    });
+    $(`.close[name=b${i}]`).bind("click", function() {
+      $(`.info-item[name=li${i}]`).attr("class", "info-item closed");
+    });
+  }
+});
