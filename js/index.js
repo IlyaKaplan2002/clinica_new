ba330f149af119dea407358cf732150147f13701
IlyaKaplan2002 <kaplan11092002@gmail.com>
2021-07-30 14:52:41 +0300

    ...

--- js/index.js
+++ js/index.js
@@ -1,17 +1,14 @@
 $(document).ready(function() {
   let items = $(".info-item");
-  console.log(items);
   for(let i = 1; i <= 5; i++) {
     $(`.open[name=b${i}]`).bind("click", function() {
       for(let j = 0; j < items.length; j++) {
         $(items[j]).attr("class", "info-item closed");
       }
       $(`.info-item[name=li${i}]`).attr("class", "info-item opened");
-      // console.log($(`.info-item[name=li${i}]`).attr("class"));
     });
     $(`.close[name=b${i}]`).bind("click", function() {
       $(`.info-item[name=li${i}]`).attr("class", "info-item closed");
     });
   }
-  $(".info-item");
 });
